f37b48c7c4d5cfdb971085cb43c6d3ff9e9d55c0
Maryanne Wachter <mclare@utsv.net>
2019-08-26 12:36:37 -0400

    added tabs and started with chips/multi dropdown

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import withStyles from '@material-ui/styles/withStyles';
-import { withRouter, Link } from 'react-router-dom';
-import CssBaseline from '@material-ui/core/CssBaseline';
+import { withRouter } from 'react-router-dom';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Slider from '@material-ui/core/Slider';
 import Typography from '@material-ui/core/Typography';
+import TabPanel from './TabPanel';
+
 
 const styles = theme => ({
   root: {
@@ -30,16 +31,49 @@ const styles = theme => ({
     padding: theme.spacing(3),
     margin: theme.spacing(2),
     textAlign: 'left',
-    color: theme.palette.text.secondary
+    color: theme.palette.text.secondary,
+    height: '100vh',
   }
 });
 
+const marks = [
+  {
+    value: 0,
+    label: '0',
+  },
+  {
+    value: 1,
+    label: '1',
+  },
+  {
+    value: 2,
+    label: '2',
+  },
+  {
+    value: 3,
+    label: '3',
+  },
+  {
+    value: 4,
+    label: '4',
+  },
+  {
+    value: 5,
+    label: '5',
+  },
+];
+
+// const tabValues = 
 
 class Dashboard extends Component {
   state = {
     amount: 15000,
     data: []
   };
+
+  handleChangeAmount = (event, value) => {
+    this.setState({amount: value, loading: false});
+  }
 
   render() {
     const { classes } = this.props;
@@ -49,31 +83,41 @@ class Dashboard extends Component {
     <React.Fragment>
       <div className={classes.root}>
         <Grid container justify="center">
-          <Grid spacing={24} alignItems="center" justify="center" container ClassName={classes.grid}>
-            <Grid item xs={12}>
-              <div className={classes.block}>
-                <Typography variant="h6" gutterBottom>ENGage Dashboard</Typography>
-                  <Typography variant="body1">
-                    Skills Matrix for Structural Engineers
-                  </Typography>
-              </div>
-            </Grid>
-            <Grid item xs={12} md={4}>
+          <Grid spacing={1} alignItems="center" justify="center" container ClassName={classes.grid}>
+            <Grid item xs={12} lg={11} md={11}>
               <Paper className={classes.paper}>
-                <div>
-                  <Typography variant="subtitle1" gutterBottom>
-                  Reinforced Concrete
-                  </Typography>
-                  <Slider
-                    value={amount}
-                    min={0}
-                    max={5}
-                    step={1}
-                  />
-                </div>
+                <Grid container justify="center">
+                  <Grid spacing={3} alignItems="center" justify="center" container ClassName={classes.grid}>
+                    <Grid item xs={12} lg={12} md={12}>
+                      <Typography variant="h6" gutterBottom>ENGage Dashboard</Typography>
+                        <Typography variant="body1">
+                          Skills Matrix for Structural Engineers
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12} md={6}>
+                      <TabPanel />
+                    </Grid>
+                    <Grid item xs={12} md={3}>
+                      <Typography variant="subtitle1" gutterBottom>
+                      Reinforced Concrete
+                      </Typography>
+                      <Slider
+                        value={amount}
+                        min={0}
+                        max={5}
+                        step={null}
+                        marks={marks}
+                        onChange={this.handleChangeAmount}
+                      />
+                    </Grid>
+                    <Grid item xs={12} md={3}>
+                      Test
+                    </Grid>
+                  </Grid>
+                </Grid>
               </Paper>
             </Grid>
-          </Grid>
+         </Grid>
         </Grid>
       </div>
     </React.Fragment>
---
+++ src/components/TabPanel.js
@@ -0,0 +1,94 @@
+import React, { Component } from 'react';
+import withStyles from '@material-ui/styles/withStyles';
+import PropTypes from 'prop-types';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Chips from './Chips';
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <Typography
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      id={`vertical-tabpanel-${index}`}
+      aria-labelledby={`vertical-tab-${index}`}
+      {...other}
+    >
+      <Box p={3}>{children}</Box>
+    </Typography>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `vertical-tab-${index}`,
+    'aria-controls': `vertical-tabpanel-${index}`,
+  };
+}
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+    display: 'flex',
+    height: 224,
+  },
+  tabs: {
+    borderRight: `1px solid ${theme.palette.divider}`,
+  },
+})
+
+class VerticalTabs extends Component {
+  state = {
+      value: 1
+  };
+
+  handleChange = (event, value) => {
+    this.setState({value: value})
+  }
+
+  render() {
+
+    const { classes } = this.props;
+    const { value } = this.state;
+    
+    return (
+      <div className={classes.root}>
+        <Tabs
+          orientation="vertical"
+          variant="scrollable"
+          value={value}
+          onChange={this.handleChange}
+          aria-label="Vertical tabs example"
+          className={classes.tabs}
+        >
+          <Tab label="Design" {...a11yProps(0)} />
+          <Tab label="Analysis" {...a11yProps(1)} />
+          <Tab label="Construction" {...a11yProps(2)} />
+        </Tabs>
+        <TabPanel value={value} index={0}>
+        <Chips />
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          Item Two
+        </TabPanel>
+        <TabPanel value={value} index={2}>
+          Item Three
+        </TabPanel>
+      </div>
+    );
+  }
+}
+
+export default withStyles(styles)(VerticalTabs)
